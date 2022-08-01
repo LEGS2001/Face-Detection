729473b38cec63182488edd8ae213e8ad1b99174
Luis Emilio <luisemiliogs@hotmail.com>
2022-08-01 01:14:51 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
-// 1. Install dependencies DONE
-// 2. Import dependencies DONE
-// 3. Setup webcam and canvas DONE
-// 4. Define references to those DONE
-// 5. Load posenet DONE
-// 6. Detect function DONE
-// 7. Drawing utilities from tensorflow DONE
-// 8. Draw functions DONE
-
-// Face Mesh - https://github.com/tensorflow/tfjs-models/tree/master/facemesh
-
 import React, { useRef, useEffect } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
-// OLD MODEL
-//import * as facemesh from "@tensorflow-models/facemesh";
 
 // NEW MODEL
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+
 import Webcam from "react-webcam";
-import { drawMesh } from "./utilities";
+import { drawMesh, drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+  })
+    .then(function(stream) {
+      const audioContext = new AudioContext();
+      const analyser = audioContext.createAnalyser();
+      const microphone = audioContext.createMediaStreamSource(stream);
+      const scriptProcessor = audioContext.createScriptProcessor(2048, 1, 1);
+  
+      analyser.smoothingTimeConstant = 0.8;
+      analyser.fftSize = 1024;
+  
+      microphone.connect(analyser);
+      analyser.connect(scriptProcessor);
+      scriptProcessor.connect(audioContext.destination);
+      scriptProcessor.onaudioprocess = function() {
+        const array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        const arraySum = array.reduce((a, value) => a + value, 0);
+        const average = arraySum / array.length;
+        const audio = Math.round(average);
+        console.log(audio)
+      };
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
   //  Load posenet
-  const runFacemesh = async () => {
-    // OLD MODEL
-    // const net = await facemesh.load({
-    //   inputResolution: { width: 640, height: 480 },
-    //   scale: 0.8,
-    // });
-    // NEW MODEL
-    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+  const run = async () => {
+    // detector objectos
+    const netCoco = await cocossd.load();
+
+    // ver si se puede encontrar algo mejor que facemesh
+    // detector facemesh
+    const netFacemesh = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+
     setInterval(() => {
-      detect(net);
+      detect(netFacemesh, netCoco);
     }, 10);
   };
 
-  const detect = async (net) => {
+  const detect = async (net1, net2) => {
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
@@ -58,19 +76,34 @@ function App() {
       canvasRef.current.height = videoHeight;
 
       // Make Detections
-      // OLD MODEL
-      //       const face = await net.estimateFaces(video);
       // NEW MODEL
-      const face = await net.estimateFaces({input:video});
-      console.log(face);
+      const faces = await net1.estimateFaces({ input: video });
+      const objects = await net2.detect(video);
+      
+          if (faces.length > 0) {
+            faces.forEach(element => {
+              console.log(element)
+            });
+          }
+      
+          if (objects.length > 0) {
+            objects.forEach(element => {
+              if (element.class == "cell phone") {
+                console.log(element)
+              }
+            });
+          }
 
       // Get canvas context
       const ctx = canvasRef.current.getContext("2d");
-      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      requestAnimationFrame(() => { drawMesh(faces, ctx); drawRect(objects, ctx); });
     }
   };
 
-  useEffect(()=>{runFacemesh()}, []);
+
+  useEffect(() => {
+    run()
+  }, []);
 
   return (
     <div className="App">
